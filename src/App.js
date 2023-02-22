dbde87f62d81b3b0f7002cf12d631478595ceeb1
Mukesh_Venkatesan <115688537+Mukesh8056@users.noreply.github.com>
2023-02-22 20:28:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import AddUser from './components/AddUser';
+import UserList from './components/UserList';
+import { AppContext } from './context';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  //actionType - (ADD, REMOVE, describes the type...)
+  //payload - the actual content
+
+  const dispatchUserEvent = (actionType, payload) => {
+    switch (actionType) {
+      case 'ADD':
+        setUsers([...users, payload.newUser]);
+        return;
+      case 'REMOVE':
+        setUsers(users.filter(user => user.id !== payload.userId));
+        return;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div className="App">
+      <AppContext.Provider value={{ users, dispatchUserEvent }}>
+        <AddUser></AddUser>
+        <UserList></UserList>
+      </AppContext.Provider>
+    </div>
+  )
+}
+
+export default App;
